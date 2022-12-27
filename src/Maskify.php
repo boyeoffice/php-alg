00ddb8dd6fdd4863a69326dd6fb0397ddfd38aaf
Olakunle Boye <kunlexzy@gmail.com>
2022-12-27 20:38:11 +0100

    removed phpunit

---
+++ src/Maskify.php
@@ -0,0 +1,21 @@
+<?php
+
+  /**
+   * Usually when you buy something, you're asked whether your credit card number,
+   * phone number or answer to your most secret question is still correct.
+   * However, since someone could look over your shoulder, you don't want that shown on your screen. Instead, we mask it.
+   * Your task is to write a function maskify, which changes all but the last four characters into '#'.
+   */
+
+function maskify($cc) {
+  $mask = str_split( $cc );
+  $maskLength = strlen( $cc ) - 5;
+  for ( $i = 0; $i <= $maskLength; $i++ ) {
+    $mask[$i] = '#';
+  }
+  
+  return implode( '', $mask );
+}
+
+echo maskify('1840234785034'); // ##########5034
+echo maskify('62847929470103'); // ###########0103
